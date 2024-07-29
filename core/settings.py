5f118b285a41c841c5cf6c25721d70759b38f15a
Shovan Kar <shovankar.dev@gmail.com>
2024-07-30 00:59:14 +0530

    Configure DRF (Django REST Framework) and DB connection (MongoDB)

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'djongo',
+        'NAME': 'bookwise',
+        'ENFORCE_SCHEMA': False,
+        'CLIENT': {
+            'host': 'mongodb+srv://bookwiseadmin:T3g2tpq3y9DgBouU@pp-cluster-1.abupvhs.mongodb.net/'
+        }  
     }
 }
 
